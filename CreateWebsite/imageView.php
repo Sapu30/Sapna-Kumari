fd69a5eaf63b3a18e9b4091b76c9e6a102729c0e
Sapna <71216375+Sapu30@users.noreply.github.com>
2023-11-09 19:24:17 +0530

    Update imageView.php

--- CreateWebsite/imageView.php
+++ CreateWebsite/imageView.php
@@ -2,7 +2,7 @@
     
 	$servername = "localhost";
 	$username = "root";
-	$password = "kanika";
+	$password = "sapna";
 	$dbname = "mywebsite";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
